a52be47c02de659dabe7ed4bcd90957ddc14eaf7
Nishant Dania <nishantdania@gmail.com>
2026-07-18 17:26:30 +0800

    Static network, multi is struggling

--- install/preflight/network.sh
+++
@@ -1,8 +0,0 @@
-command -v dnsmasq >/dev/null && return
-
-gum log --level info "Installing Firecracker network tools..."
-if ((EUID == 0)); then
-    pacman -S --needed --noconfirm dnsmasq nftables iproute2
-else
-    sudo pacman -S --needed --noconfirm dnsmasq nftables iproute2
-fi
