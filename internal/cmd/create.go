d7c4bdcd2e49ebce1ced206e4820d887474c9c3e
Nishant Dania <nishantdania@gmail.com>
2026-08-23 17:22:55 +0800

    Share boilerplate

--- internal/cmd/create.go
+++ internal/cmd/create.go
@@ -1,36 +1,11 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 
-	"github.com/nishantdania/ark/internal/api"
 	"github.com/nishantdania/ark/internal/client"
 )
 
 func newCreateCmd(options *rootOptions) *cobra.Command {
-	return &cobra.Command{
-		Use:   "create <name>",
-		Short: "Create an Ark",
-		Args:  cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			arkdClient, err := client.New(options.serverURL)
-			if err != nil {
-				return fmt.Errorf("create arkd client: %w", err)
-			}
-
-			created, err := arkdClient.CreateArk(cmd.Context(), args[0])
-			if err != nil {
-				return err
-			}
-
-			writer, err := newOutputWriter(options, cmd.OutOrStdout())
-			if err != nil {
-				return err
-			}
-
-			return writer.Write(created, arkTable([]api.Ark{created}))
-		},
-	}
+	return newArkNameCmd(options, "create <name>", "Create an Ark", (*client.Client).CreateArk)
 }
--- internal/cmd/delete.go
+++ internal/cmd/delete.go
@@ -1,36 +1,11 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 
-	"github.com/nishantdania/ark/internal/api"
 	"github.com/nishantdania/ark/internal/client"
 )
 
 func newDeleteCmd(options *rootOptions) *cobra.Command {
-	return &cobra.Command{
-		Use:   "delete <name>",
-		Short: "Delete an Ark",
-		Args:  cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			arkdClient, err := client.New(options.serverURL)
-			if err != nil {
-				return fmt.Errorf("create arkd client: %w", err)
-			}
-
-			deleted, err := arkdClient.DeleteArk(cmd.Context(), args[0])
-			if err != nil {
-				return err
-			}
-
-			writer, err := newOutputWriter(options, cmd.OutOrStdout())
-			if err != nil {
-				return err
-			}
-
-			return writer.Write(deleted, arkTable([]api.Ark{deleted}))
-		},
-	}
+	return newArkNameCmd(options, "delete <name>", "Delete an Ark", (*client.Client).DeleteArk)
 }
